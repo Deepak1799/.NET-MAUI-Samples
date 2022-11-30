a581cc50e8e444a54b358321a2144558e0aed83e
Deepak G <65169077+Deepak1799@users.noreply.github.com>
2022-12-01 05:24:47 +0530

    Android image quality issue

---
+++ AndroidImageQualityLoss/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace AndroidImageQualityLoss;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		MyImage.HeightRequest = 180;
+		MyImage.WidthRequest = 120;
+    }
+}
---
+++ AndroidImageQualityLoss/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AndroidImageQualityLoss;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
