c0bee5f3556adb3f53dbebc0dd2184f3ca0c95f4
Deepak G <65169077+Deepak1799@users.noreply.github.com>
2023-03-06 17:49:45 +0530

    "ClearButtonVisibility" property is not working properly when opening the Entry at run time
    Added demo for the issue in Windows => "ClearButtonVisibility" property is not working properly when opening the Entry at run time.

---
+++ EntryClearIcon/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EntryClearIcon;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ EntryClearIcon/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace EntryClearIcon;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        TextInput.IsVisible = !TextInput.IsVisible;
+    }
+}
+
